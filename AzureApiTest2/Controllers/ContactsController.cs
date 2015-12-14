e63970de08518ba5bc86abd30aae5afc48ab8656
Dan Black <dblack@boardbooks.com>
2015-12-14 15:41:46 +1300

    initial commit

---
+++ AzureApiTest2/Controllers/ContactsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AzureApiTest2.Models;
+
+namespace AzureApiTest2.Controllers
+{
+    public class ContactsController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Contact> Get()
+        {
+            return new Contact[]{
+                
+                new Contact { Id = 1, EmailAddress = "barney@contoso.com", Name = "Barney Poland"},
+                new Contact { Id = 2, EmailAddress = "lacy@contoso.com", Name = "Lacy Barrera"},
+                new Contact { Id = 3, EmailAddress = "lora@microsoft.com", Name = "Lora Riggs"},
+                new Contact { Id = 4, EmailAddress = "deploy@microsoft.com", Name = "Deploy Tester"}
+            };
+        }
+    }
+}
---
+++ AzureApiTest2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureApiTest2.WebApiApplication" Language="C#" %>
