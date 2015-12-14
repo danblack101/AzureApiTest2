1ba235f8e74e321a95f7214384ca58a3119cc3ab
Dan Black <dblack@boardbooks.com>
2015-12-14 16:53:53 +1300

    add xmas controller

---
+++ AzureApiTest2/Controllers/XmasController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace AzureApiTest2.Controllers
+{
+    public class XmasController : ApiController
+    {
+        [HttpGet]
+        public string OnTheNthDay(int day)
+        {
+            return "On the First day of Christmas my true love sent to me a Partridge in a Pear Tree.";
+        }
+    }
+}
